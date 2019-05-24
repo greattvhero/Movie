81bbd8559e7ad54b4f6bfdf91783abd5fa992d4d
Greattvhero <greattvhero@gmail.com>
2019-05-24 10:31:30 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'movie.rb'
-require_relative 'movie_collection.rb'
-movie = MovieCollection.new('movies.txt')
 require_relative 'video.rb'
 require_relative 'video_collection.rb'
 movie = VideoCollection.new('movies.txt')
