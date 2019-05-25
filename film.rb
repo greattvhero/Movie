eed3f0029d8b0c8d7084a39931166b705de20d91
Greattvhero <greattvhero@gmail.com>
2019-05-25 19:19:56 +0300

    Adding initialize to film.rb

--- film.rb
+++ film.rb
@@ -3,7 +3,17 @@ require_relative 'video'
 class Film < Video
   attr_accessor :colors
 
-    def initialize
+    def initialize(link, movie, year, country, showing, jenre, length, value, director, actor, colors)
+      @link = link
+      @movie = movie
+      @year = year
+      @country = country
+      @showing = showing
+      @jenre = jenre.split(',')
+      @length = length
+      @value = value
+      @director = director
+      @actor = actor
       @colors = colors
     end
 end
