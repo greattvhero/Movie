b71619e08689d7583a341bdbd56b4f426933eb04
Greattvhero <greattvhero@gmail.com>
2019-05-29 15:43:17 +0300

    Fixed file video.rb

--- film.rb
+++ film.rb
@@ -4,8 +4,6 @@ class Film < Video
   attr_accessor :link, :movie, :year, :country, :showing, :jenre,
                 :length, :value, :director, :actor, :colors
 
-    def initialize(a:, b:, c:, d:, e:, f:, g:, h:, i:, j:, k:)
-      puts "#{a} #{b} #{c} #{d} #{e} #{f} #{g} #{h} #{i} #{j} #{k}"
+    def initialize(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:, colors:)
     end
-  initialize(a: link, b: movie, c: year, d: country, e: showing, f: jenre, g: length, h: value, i: director, j: actor, k: colors)
 end
