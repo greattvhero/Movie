babd3b3e7a2e6f09fdd829928c15a89c244b6eb7
Greattvhero <greattvhero@gmail.com>
2019-05-29 17:17:25 +0300

    Added params

--- video.rb
+++ video.rb
@@ -4,7 +4,8 @@ class Video
   attr_accessor :link, :movie, :year, :country, :showing, :jenre,
                 :length, :value, :director, :actor
 
-  def initialize(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:)
+  def initialize(params[0], params[1], params[2], params[3],
+                          params[4], params[5], params[6], params[7], params[8], params[9])
     @link = link
     @movie = movie
     @year = year
