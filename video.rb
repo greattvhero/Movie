7ac94bf76e29a27181358370f9e5498b7d7dd54a
Greattvhero <greattvhero@gmail.com>
2019-05-29 18:58:03 +0300

    Fixed files

--- video.rb
+++ video.rb
@@ -4,8 +4,7 @@ class Video
   attr_accessor :link, :movie, :year, :country, :showing, :jenre,
                 :length, :value, :director, :actor
 
-  def initialize(params[0], params[1], params[2], params[3],
-                          params[4], params[5], params[6], params[7], params[8], params[9])
+  def initialize(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:)
     @link = link
     @movie = movie
     @year = year
--- video_collection.rb
+++ video_collection.rb
@@ -13,8 +13,8 @@ class VideoCollection
   def read_file
     File.readlines(@file).each do |line|
       params = line.split('|')
-      new_movie = ::Video.new(params[0], params[1], params[2], params[3],
-                              params[4], params[5], params[6], params[7], params[8], params[9])
+      new_movie = ::Video.new(link:, movie:, year:, country:, showing:,
+        jenre:, length:, value:, director:, actor:)
       @movies.push(new_movie)
     end
   end
