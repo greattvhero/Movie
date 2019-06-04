24777193dad09443c8053c712280c3eba7552f0a
Greattvhero <greattvhero@gmail.com>
2019-06-04 18:18:12 +0300

    Fixing

--- film.rb
+++ film.rb
@@ -1,9 +1,9 @@
 require_relative 'video'
 
 class Film < Video
-  attr_accessor colors:
+  attr_accessor :colors
 
-  def initialize(colors:)
+  def initialize
     super
     @colors = colors
   end
