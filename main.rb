9243e850ab36c4096e4ca0247ab1a34429a3be93
Greattvhero <greattvhero@gmail.com>
2019-06-05 18:26:32 +0300

    Fixing

--- main.rb
+++ main.rb
@@ -3,8 +3,8 @@
 require_relative 'video'
 require_relative 'film'
 require_relative 'video_collection'
-movie = Film.new("http://imdb.com/title/tt1555149/?ref_=chttp_tt_250",
-  "Elite Squad: The Enemy Within", "2010", "Brazil", "2010-10-08",
-  "Action,Crime,Drama", "115 min", "8.1" ,"José Padilha",
-  "Wagner Moura,Irandhir Santos,André Ramiro")
+movie = Film.new(link: "http://imdb.com/title/tt1555149/?ref_=chttp_tt_250",
+  movie: "Elite Squad: The Enemy Within", year: "2010", country: "Brazil", showing: "2010-10-08",
+  jenre: "Action,Crime,Drama", length: "115 min", value: "8.1", director: "José Padilha",
+  actor: "Wagner Moura,Irandhir Santos,André Ramiro")
   puts movie.inspect
