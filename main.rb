909d280a6de1070dee03459a5aef085e9381fe10
Greattvhero <greattvhero@gmail.com>
2019-06-06 16:02:51 +0300

    Adding new file

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 
 require_relative 'video'
 require_relative 'film'
+require_relative 'series'
 require_relative 'video_collection'
 movie = Film.new(link: "http://imdb.com/title/tt1555149/?ref_=chttp_tt_250",
   movie: "Elite Squad: The Enemy Within", year: "2010", country: "Brazil", showing: "2010-10-08",
---
+++ series.rb
@@ -0,0 +1,10 @@
+require_relative 'video'
+
+class Series < Video
+  attr_accessor :seasons
+
+  def initializel(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:, seasons:)
+    super
+    @seasons = seasons
+  end
+end
