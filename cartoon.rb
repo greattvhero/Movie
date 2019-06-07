407e5247f78d2b2119ed0aafcdd74c1501602f57
Greattvhero <greattvhero@gmail.com>
2019-06-07 18:40:52 +0300

    Adding new file

---
+++ cartoon.rb
@@ -0,0 +1,24 @@
+require_relative 'video'
+
+class Cartoons < Video
+  attr_accessor :dimension
+
+  def initializel(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:, dimension:)
+    super
+    @dimension = dimension
+  end
+
+  def other_dimensions
+    DIMENSION = [if jenre.include?("Animation") && year >= year.include?("1995")
+                  @dimension = "This animated movie is 3D"
+                  puts @dimension
+                  if movie.include?("Toy Story")
+                    @dimension = "This animated movie is Toy Story"
+                    puts @dimension
+                  else
+                    @dimension = "This animated movie isn't Toy Story"
+                    puts @dimension
+                  end
+                else
+                  @dimension = "This animated movie is 2D"
+                  puts @dimension]
--- main.rb
+++ main.rb
@@ -4,7 +4,8 @@ require_relative 'video'
 require_relative 'film'
 require_relative 'series'
 require_relative 'video_collection'
-movie = Film.new(link: "http://imdb.com/title/tt1555149/?ref_=chttp_tt_250",
+
+movie = Series.new(link: "http://imdb.com/title/tt1555149/?ref_=chttp_tt_250",
   movie: "Elite Squad: The Enemy Within", year: "2010", country: "Brazil", showing: "2010-10-08",
   jenre: "Action,Crime,Drama", length: "115 min", value: "8.1", director: "José Padilha",
   actor: "Wagner Moura,Irandhir Santos,André Ramiro")
