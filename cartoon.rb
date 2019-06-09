788fb0897db08caff72472c6c0c0ebeea207a7c2
Greattvhero <greattvhero@gmail.com>
2019-06-09 16:45:53 +0300

    Fixing

--- cartoon.rb
+++ cartoon.rb
@@ -1,25 +1,11 @@
 require_relative 'video'
 
-class Cartoons < Video
+class Cartoon < Video
   attr_accessor :dimension
 
   def initializel(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:, dimension:)
     super
     @dimension = dimension
+    @dimension = ["2D","3D"]
   end
-
-  def other_dimensions
-    DIMENSION = [if jenre.include?("Animation") && year >= year.include?("1995")
-                  @dimension = "This animated movie is 3D"
-                  puts @dimension
-                  if movie.include?("Toy Story")
-                    @dimension = "This animated movie is Toy Story"
-                    puts @dimension
-                  else
-                    @dimension = "This animated movie isn't Toy Story"
-                    puts @dimension
-                  end
-                else
-                  @dimension = "This animated movie is 2D"
-                  puts @dimension]
-    end
+end
--- main.rb
+++ main.rb
@@ -3,10 +3,11 @@
 require_relative 'video'
 require_relative 'film'
 require_relative 'series'
+require_relative 'cartoon'
 require_relative 'video_collection'
 
-movie = Series.new(link: "http://imdb.com/title/tt1555149/?ref_=chttp_tt_250",
-  movie: "Elite Squad: The Enemy Within", year: "2010", country: "Brazil", showing: "2010-10-08",
-  jenre: "Action,Crime,Drama", length: "115 min", value: "8.1", director: "José Padilha",
-  actor: "Wagner Moura,Irandhir Santos,André Ramiro")
+movie = Cartoon.new(link: "http://imdb.com/title/tt0910970/?ref_=chttp_tt_62",
+  movie: "WALL·E", year: "2008", country: "USA", showing: "2008-06-27",
+  jenre: "Animation,Adventure,Family", length: "98 min", value: "8.4", director: "Andrew Stanton",
+  actor: "Ben Burtt,Elissa Knight,Jeff Garlin")
   puts movie.inspect
