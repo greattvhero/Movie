55b717717656e2f0d1b24a253bb8ff2c2c9b04de
Greattvhero <greattvhero@gmail.com>
2019-06-11 16:35:11 +0300

    Fixing

--- cartoon.rb
+++ cartoon.rb
@@ -7,7 +7,9 @@ class Cartoon < Video
   def initializel(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:, dimension:)
     super
     if dimension == "2D" || "3D"
-      DIMENSION.include?(dimension)
+      unless DIMENSION.include?(dimension)
+        raise fail ArgumentError, "dimension is not valid"
+      end
     end
   end
 end
