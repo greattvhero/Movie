309ba0a2b5cb4293daaeb0321d3320eedd2f4ccc
Greattvhero <greattvhero@gmail.com>
2019-06-11 14:57:09 +0300

    Fixing

--- cartoon.rb
+++ cartoon.rb
@@ -2,6 +2,7 @@ require_relative 'video'
 
 class Cartoon < Video
   attr_accessor :dimension
+  DIMENSION = dimension
 
   def initializel(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:, dimension:)
     super
