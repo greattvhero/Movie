b779b43a7d70d57877bf4546588f9d8791875528
Greattvhero <greattvhero@gmail.com>
2019-08-02 14:37:46 +0300

    FIxed dimension

--- cartoon.rb
+++ cartoon.rb
@@ -6,7 +6,7 @@ class Cartoon < Video
 
   def initializel(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:, dimension:)
     super
-    dimension = DIMENSION
+    @dimension = dimension
       if dimension.include?(DIMENSION)
         raise ('fail ArgumentError, "dimension is not valid"')
       end
