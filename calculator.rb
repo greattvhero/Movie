b06a7da0e2535f9c6d8c13030a6e23cabc33ea4a
Greattvhero <greattvhero@gmail.com>
2019-08-08 13:28:09 +0300

    FIxing

--- calculator.rb
+++ calculator.rb
@@ -2,22 +2,22 @@ class Calculator
 
   def sum(x,y)
     check_types!(x,y)
-      x + y
+    x + y
   end
 
   def div(x,y)
     check_types!(x,y)
-      x / y
+    x / y
   end
 
   def multiply(x,y)
     check_types!(x,y)
-      x * y
+    x * y
   end
 
   def substract(x,y)
     check_types!(x,y)
-      x - y
+    x - y
   end
 
   def eql(x,y)
