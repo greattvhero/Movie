66b52573472a57ec4100e0b46e591a5b9e562e13
Greattvhero <greattvhero@gmail.com>
2019-08-14 10:12:19 +0300

    Fixing

--- main.rb
+++ main.rb
@@ -12,12 +12,5 @@ movie: "The Shining", year: "1980", country: "USA", showing: "1980-05-23", jenre
 length: "146 min", value: "8.4", director: "Stanley Kubrick",
 actor: "Jack Nicholson,Shelley Duvall,Danny Lloyd", colors: "red")
 
-calculator = Calculator.new
-puts calculator.sum(16,4).inspect
-puts calculator.div(16,4).inspect
-puts calculator.multiply(16,4).inspect
-puts calculator.substract(16,4).inspect
-puts calculator.eql(5,4).inspect
-
 video = VideoCollection.new("movies.txt")
-puts video.cartoons.inspect
+puts video.series.inspect
