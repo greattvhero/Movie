6d4b787117b211812d889d29ed399814d1e4032f
Greattvhero <greattvhero@gmail.com>
2019-08-23 13:41:37 +0300

    Fixing

--- main.rb
+++ main.rb
@@ -11,8 +11,10 @@ require_relative 'checkout'
 product = Product.new(cost: 10, product: "3 bottles of water")
 product_2 = Product.new(cost: 7, product: "5 packs of crisps")
 product_3 = Product.new(cost: 30, product: "2 bars of chockolate")
+product_4 = Product.new(cost: 60, product: "2 bars of kinder chockolate")
 basket = Checkout.new
 basket.add(product)
 basket.add(product_2)
 basket.add(product_3)
+basket.add(product_4)
 puts basket.sum
