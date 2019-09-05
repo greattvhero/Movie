ca63b5f21db7dc5d3d6a696d4b43e60ee17776ef
Greattvhero <greattvhero@gmail.com>
2019-09-05 10:50:32 +0300

    Fixing

--- social_network.rb
+++ social_network.rb
@@ -33,19 +33,19 @@ class SocialNetwork
   end
 
   def men
-    @gender.select{|profile| profile.class == GENDER.include?(male)}
+    @profiles.select{|profile| profile.gender == 'male'}
   end
 
   def women
-    @gender.select{|profile| profile.class == GENDER.include?(female)}
+    @profiles.select{|profile| profile.gender == 'female'}
   end
 
   def teenagers
-    @gender.select{|profile| profile.class == GENDER.include?(teenagers)}
+    @profiles.select{|profile| profile.birthdate < Date.parse("28.03.2001")}
   end
 
   def grownups
-    @profiles.select{|profile| profile.class == GENDER.include?(grownups)}
+    @profiles.select{|profile| profile.birthdate > Date.parse("28.03.2001")}
   end
 end
 
@@ -55,6 +55,15 @@ profile = Profile.new(full_name: "Fedor Borislavovich Repeturov",
 profile_2 = Profile.new(full_name: "Borislav Ivanovich Shestenuk", birthdate: ::DateTime.parse("12.04.1984"),
   gender: 'male', city: "Kyiv", country: "Ukraine",
 phone: "38-060-256-5637")
+profile_3 = Profile.new(full_name: "Evgeniy Leonidovich Padun",
+  birthdate: ::DateTime.parse("28.03.1999"),
+  gender: 'male', city: "Kyiv", country: "Ucraine", phone: "38-050-333-4256")
+profile_4 = Profile.new(full_name: "Lola Fedorovna Oxxymironova",
+  birthdate: ::DateTime.parse("8.01.1996"),
+  gender: 'female', city: "Kyiv", country: "Ucraine", phone: "38-050-333-4256")
+profile_5 = Profile.new(full_name: "Bogdan Leonidovich Padun",
+  birthdate: ::DateTime.parse("04.10.1998"),
+  gender: 'male', city: "Kyiv", country: "Ucraine", phone: "38-050-333-4256")
 social_network = SocialNetwork.new
 
-puts profile.inspect
+puts social_network.women.inspect
