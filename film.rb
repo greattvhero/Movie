ee68168e2bee57cea9d90d45e5b3c1cee5bc20cc
Greattvhero <greattvhero@gmail.com>
2019-09-11 11:03:12 +0300

    FIxing

--- film.rb
+++ film.rb
@@ -1,14 +1,1 @@
-require_relative 'video'
-
-class Film < Video
-  attr_accessor :colors
-
-  def initialize(link:, movie:, year:, country:, showing:, jenre:, length:, value:, director:, actor:, colors:)
-    super
-    @colors = colors
-  end
-
-  def info
-    print "I am Film"
-  end
-end
+https://github.com/greattvhero/Movie.git
