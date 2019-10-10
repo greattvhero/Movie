0b68ec3a0cace06afe41711b21c517b52518b295
Greattvhero <greattvhero@gmail.com>
2019-10-10 12:35:32 +0300

    Fixing

--- social_network.rb
+++ social_network.rb
@@ -1,5 +1,6 @@
+require 'date'
 class Profile
-  attr_accessor: :full_name, :birthdate, :gender, :city, :country, :phone
+  attr_accessor :full_name, :birthdate, :gender, :city, :country, :phone
 
   def initialize(full_name:, birthdate:, gender:, city:, country:, phone:)
     @full_name = full_name
@@ -9,3 +10,23 @@ class Profile
     @country = country
     @phone = phone
   end
+
+  def gender
+    if @gender == "male"
+      puts "Gender is male"
+    elsif @gender == "female"
+      puts "Gender is female"
+    end
+  end
+
+  def birthdate
+    if @birthdate == ::DateTime.new("28.03.1999")
+      puts "Birthdate validate"
+    elsif @birthdate != ::DateTime.new("28.03.1999")
+      puts "Birthdate not validate"
+    end
+  end
+end
+
+profile = Profile.new(full_name: "Evgeniy Padun", birthdate: "28.03.1999", gender: "male", city: "Kyiv", country: "Ucraine", phone:"+38 050 678 27 03")
+puts profile.birthdate
